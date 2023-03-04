9b9c422f9fe4810198b11ac0008cc7a1a2671972
smajaas <smajaas@gmail.com>
2023-03-04 23:51:41 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Final from './react-projects/02-useEffect/final/04-fetch-data';
+import Final from './react-projects/03-conditional-rendering/final/02-multiple-returns-fetch-data';
 
 function App() {
   return (
--- src/react-projects/03-conditional-rendering/final/02-multiple-returns-fetch-data.jsx
+++ src/react-projects/03-conditional-rendering/final/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,29 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setIsLoading] = useState[true];
+  const [isError, setIsError] = useState[false];
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const resp = await fetch(url);
+        const user = await resp.json();
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+  return <h2>hai</h2>;
 };
 export default MultipleReturnsFetchData;
