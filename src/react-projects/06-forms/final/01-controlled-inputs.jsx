99458aba7b1b5898baf39e72e964710fef900cf4
smajaas <smajaas@gmail.com>
2023-03-09 19:52:17 +0530

    cntrolled inputs

--- src/react-projects/06-forms/final/01-controlled-inputs.jsx
+++ src/react-projects/06-forms/final/01-controlled-inputs.jsx
@@ -1,18 +1,41 @@
+import { useState } from 'react';
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //do sumthing
+    console.log(name, email);
+  };
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={handleSubmit}>
       <h4>Controlled Inputs</h4>
       <div className='form-row'>
         <label htmlFor='name' className='form-label'>
           name
         </label>
-        <input type='text' id='name' className='form-input' />
+        <input
+          type='text'
+          id='name'
+          className='form-input'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className='form-row'>
         <label htmlFor='email' className='form-label'>
           email
         </label>
-        <input type='email' id='email' className='form-input' />
+        <input
+          type='email'
+          id='email'
+          className='form-input'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <button type='submit' className='btn btn-block'>
           submit
         </button>
