a91cf0d02443381901c20f00d0249197c7f731e5
smajaas <smajaas@gmail.com>
2023-05-16 17:09:34 +0530

    lowerstate down

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Final from './react-projects/10-useReducer/final/01-useReducer';
+import Final from './react-projects/11-performance/starter/02-lower-state-challenge';
 
 function App() {
   return (
